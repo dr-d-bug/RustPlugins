6339da39dda4a7ca6c0a45a73ac68ae46dfdef2d
dr-d-bug <49510052+dr-d-bug@users.noreply.github.com>
2021-04-03 04:16:04 +0200

    1st release

---
+++ RustTools.cs
@@ -0,0 +1,41 @@
+﻿using Oxide.Core.Libraries.Covalence;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oxide.Plugins
+{
+    [Info("RustTools", "Dr.D.Bug", "0.0.1")]
+
+    class RustTools : RustPlugin
+    {
+        public Dictionary<ulong, string> activplayers = new Dictionary<ulong, string>();
+
+        void Init()
+        {
+            // Puts("RustTools geladen")
+        }
+
+        void SendMessage(BasePlayer player, string msg, params object[] args)
+        {
+            PrintToChat(player, msg, args);
+        }
+
+        [ChatCommand("players")]
+        void player(BasePlayer player)
+        {
+            activplayers.Clear();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Players online: ");
+            List<BasePlayer> list = BasePlayer.activePlayerList.ToList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                BasePlayer basePlayer = list[i];
+                // SendMessage(player, $"{basePlayer.displayName} ");
+                sb.Append($"{basePlayer.displayName} ");
+            }
+            SendMessage(player, sb.ToString());
+        }
+
+    }
+}
